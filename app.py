d01fa8f95da5ea1fb413552ee796f9e7a914c93d
marccosabino <marco.sabinosr@gmail.com>
2025-04-04 12:21:49 -0300

    Update

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 print("""
 ░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
 ██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
@@ -8,9 +9,24 @@ print("""
 """)
 
 print('1. Cadastrar restaurante')
-print('2. Listar restaurante')
+print('2. Listar restaurantes')
 print('3. Ativar restaurante')
 print('4. Sair\n')
 
-opcao_escolhida = input('Escolha uma opção: ')
-print(f'Você escolheu a opção {opcao_escolhida}')
+opcao_escolhida = int(input('Escolha uma opção: '))
+
+def finalizar_app():
+    print('Finalizando o app')
+
+# If e else
+if opcao_escolhida == 1:
+    print('Cadastrar restaurante')
+elif opcao_escolhida == 2:
+    print('Listar restaurantes')
+elif opcao_escolhida == 3:
+    print('Ativar restaurante')
+else:
+    finalizar_app()
+    #os.system('cls') COMANDO PARA O WINDOWS
+    os.system('clear')
+    print('Finalizando o app\n')
