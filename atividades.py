1ee9195384fb814fffabd1b1ecce5d8b1abb7cab
marccosabino <marco.sabinosr@gmail.com>
2025-04-04 11:50:58 -0300

    Update

---
+++ atividades.py
@@ -0,0 +1,20 @@
+# Atividade 1 
+print('Python na Escola de Programação da Alura')
+
+# Atividade 2
+nome = 'Marco'
+idade = 20
+print(f'Meu nome é {nome} e tenho {idade} anos')
+
+# Atividade 3
+print('''
+A
+L
+U
+R
+A 
+      ''')
+
+# Atividade 4 
+pi = 3.14159
+print(f'O valor arredondado de pi é: {pi:.2f}')
