e348a31c8403e9385af50f575aad59006baddd54
marccosabino <marco.sabinosr@gmail.com>
2025-05-03 19:04:36 -0300

    Update

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 
 restaurantes = ['Pizza', 'Sushi']
 
-def exibir_nome_programa():
+def exibir_nome_do_programa():
     print("""
 ░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
 ██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
@@ -12,67 +12,67 @@ def exibir_nome_programa():
 ╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░  
 """)
 
-
 def exibir_opcoes():
     print('1. Cadastrar restaurante')
     print('2. Listar restaurantes')
     print('3. Ativar restaurante')
     print('4. Sair\n')
 
-
 def finalizar_app():
-    os.system('cls')
-    print('Finalizando o app\n')
+    exibir_subtitulo('Finalizar app')
 
+def voltar_ao_menu_principal():
+    input('\nDigite uma tecla para voltar ao menu ')
+    main()
 
 def opcao_invalida():
     print('Opção inválida!\n')
-    input('Digite uma tecla para voltar ao menu principal')
-    main()
+    voltar_ao_menu_principal()
 
+def exibir_subtitulo(texto):
+    os.system('cls')
+    print(texto)
+    print()
 
 def cadastrar_novo_restaurante():
-    os.system('cls')
-    print('Cadastro de novos restaurantes\n')
-    nome_do_restaurante = input('Digite o nome do restaurante que deseja cadastrar:')
+    exibir_subtitulo('Cadastro de novos restaurantes')
+    nome_do_restaurante = input('Digite o nome do restaurante que deseja cadastrar: ')
     restaurantes.append(nome_do_restaurante)
     print(f'O restaurante {nome_do_restaurante} foi cadastrado com sucesso!')
-    input () ('Digite uma tecla para voltar ao menu principal.')
-    main()
-
+    
+    voltar_ao_menu_principal()
 
 def listar_restaurantes():
-    os.system('cls')
-    print('Listando restaurantes\n')
+    exibir_subtitulo('Listando restaurantes')
+
     for restaurante in restaurantes:
         print(f'.{restaurante}')
 
-    input('\nDigite uma tecla para voltar ao menu principal')
-    main()
-
+    voltar_ao_menu_principal()
 
 def escolher_opcao():
     try:
         opcao_escolhida = int(input('Escolha uma opção: '))
+        # opcao_escolhida = int(opcao_escolhida)
 
-        if opcao_escolhida == 1:
+        if opcao_escolhida == 1: 
             cadastrar_novo_restaurante()
-        elif opcao_escolhida == 2:
-            lista_restaurantes()
-        elif opcao_escolhida == 3:
+        elif opcao_escolhida == 2: 
+            listar_restaurantes()
+        elif opcao_escolhida == 3: 
             print('Ativar restaurante')
-        elif opcao_escolhida == 4:
+        elif opcao_escolhida == 4: 
             finalizar_app()
-        else:
+        else: 
             opcao_invalida()
     except:
-        opcao_invalida()        
-    
+        opcao_invalida()
+
 def main():
     os.system('cls')
-    exibir_nome_programa()
+    exibir_nome_do_programa()
     exibir_opcoes()
     escolher_opcao()
 
 if __name__ == '__main__':
-    main(   )
+    main()
--- atividades.py
+++ atividades.py
@@ -18,3 +18,7 @@ A
 # Atividade 4 
 pi = 3.14159
 print(f'O valor arredondado de pi é: {pi:.2f}')
+
+#Atividade 5
+numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+nome = ['Marco', 'Maria', 'João', 'Pedro']
