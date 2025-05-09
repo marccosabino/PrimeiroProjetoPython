f9ce950d6a34d95b51821ae01bc130b9af61722d
marccosabino <marco.sabinosr@gmail.com>
2025-05-09 13:51:52 -0300

    Update

---
+++ atividade_didionario.py
@@ -0,0 +1,6 @@
+#1 - Crie um dicionário representando informações sobre uma pessoa, como nome, idade e cidade.
+usuarios = {'Nome': 'João', 'Idade':'30', 'Cidade':'Salvador'}
+print(usuarios)
+
+#2
+usuarios.upadte({'Idade':'29'})
